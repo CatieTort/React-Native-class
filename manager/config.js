e397b72bca944121b64f9b3ebf056c59d2957c00
CatieTort <catietortorella@gmail.com>
2018-07-31 09:51:11 -0700

    added config file

---
+++ manager/config.js
@@ -0,0 +1,8 @@
+export const config = {
+	apiKey: "AIzaSyC1CikTBiNtL5tJthjxK67O5YjuBe7sZNk",
+	authDomain: "managerapp-reactnative.firebaseapp.com",
+	databaseURL: "https://managerapp-reactnative.firebaseio.com",
+	projectId: "managerapp-reactnative",
+	storageBucket: "managerapp-reactnative.appspot.com",
+	messagingSenderId: "245579089862"
+  }
--- manager/src/App.js
+++ manager/src/App.js
@@ -6,18 +6,11 @@ import { Header } from './Components/common';
 import LoginForm from './Components/LoginForm';
 import reducers from './reducers';
 import firebase from 'firebase'
+import config from "../config.js"
 
 
 class App extends Component {
 	componentWillMount(){
-		const config = {
-		    apiKey: "AIzaSyC1CikTBiNtL5tJthjxK67O5YjuBe7sZNk",
-		    authDomain: "managerapp-reactnative.firebaseapp.com",
-		    databaseURL: "https://managerapp-reactnative.firebaseio.com",
-		    projectId: "managerapp-reactnative",
-		    storageBucket: "managerapp-reactnative.appspot.com",
-		    messagingSenderId: "245579089862"
-		  };
   		firebase.initializeApp(config);
 	}
 
